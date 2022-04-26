5237f144c3b460b08dd90399b00bfd16b9f2f427
valentimarco <gamespy67@gmail.com>
2022-04-26 12:58:31 +0200

    BackEnd completed

--- SoundMixerplugin/Program.cs
+++ SoundMixerplugin/Program.cs
@@ -4,13 +4,20 @@ namespace SoundMixer{
     class SoundMixer{
         public static void Main(string[] args){
             IDictionary<string,int> devices = AudioManager.GetAllVolumeObjects();
-            
-            
+            Console.WriteLine("Process with sound Active");
             foreach (var process in devices) {
-                Console.Write("Process name:" + process.Key);
-                Console.Write(", Volume:" + AudioManager.GetApplicationVolume(process.Value));
-                Console.Write("\n");
+                    Console.Write("Process name: " + process.Key);
+                    Console.Write(",Volume:" + AudioManager.GetApplicationVolume(process.Value));
+                    Console.Write(" ,Mute? " + AudioManager.GetApplicationMute(process.Value));
+                    Console.Write("\n");
             }
+                
+            
+            
+            
+            
+            
+            
             
             
         }
