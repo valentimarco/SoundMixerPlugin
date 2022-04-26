07393444ab5978ce4ab81cf06378a0778d47d9ee
valentimarco <gamespy67@gmail.com>
2022-04-26 11:38:12 +0200

    Added file that github refuses to upload xddd

---
+++ SoundMixerplugin/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CoreAudio;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+
+namespace SoundMixer{
+    class SoundMixer{
+        public static void Main(string[] args){
+            IList<string> devices = AudioManager.GetAllVolumeObjects();
+            foreach(var device in devices) Console.WriteLine(device);
+        }
+
+
+
+       
+    }
+}
+
+
+
