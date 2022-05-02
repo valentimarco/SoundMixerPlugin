727f02884ef5bd2a69bfc2a89659f82ddee10ace
valentimarco <gamespy67@gmail.com>
2022-05-02 19:11:45 +0200

    Created OpenGl texture

--- SoundMixerplugin/Overlay/OverlayVR.cs
+++ SoundMixerplugin/Overlay/OverlayVR.cs
@@ -7,14 +7,15 @@ namespace SoundMixer {
     public class OverlayVR : Application {
         private Overlay _overlay;
         private string _keyoverlay = "soundmixer";
-        private IntPtr _OverlayTexture;
+        private IntPtr _OverlayTexture; //TODO: creare Ui sfruttando vulkan
+        private Window _window = new Window(1920,1080);
         public OverlayVR(string? debug = null) : base(ApplicationType.Overlay) {
             _overlay = new Overlay(_keyoverlay, "SoundMixer", true);
             _overlay.WidthInMeters = 2f;
             _overlay.SetTexture(new Texture_t {
                 eColorSpace = EColorSpace.Auto,
-                eType = ETextureType.Vulkan,
-                handle = _OverlayTexture
+                eType = ETextureType.OpenGL,
+                handle = new IntPtr(_window.TextureId)
             });
             OpenVR.Overlay.SetOverlayInputMethod(_overlay.Handle, VROverlayInputMethod.Mouse);
             _overlay.StartPolling();
@@ -25,7 +26,6 @@ namespace SoundMixer {
 
         ~OverlayVR() {
             _overlay.Destroy();
-            GC.SuppressFinalize(this);
         }
 
         
